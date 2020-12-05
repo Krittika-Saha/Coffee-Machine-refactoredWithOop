2907dbd1f042f3baafef066be1c49405f9a9ddee
Krittika-Saha <krittika.saha.dev@gmail.com>
2020-12-05 15:29:11 +0530

    finished project

--- coffee_maker.py
+++ coffee_maker.py
@@ -1,3 +1,4 @@
+from time import sleep
 class CoffeeMaker:
     """Models the machine that makes the coffee"""
     def __init__(self):
@@ -26,4 +27,6 @@ class CoffeeMaker:
         """Deducts the required ingredients from the resources."""
         for item in order.ingredients:
             self.resources[item] -= order.ingredients[item]
+        print("Making you a coffee...")
+        sleep(2)
         print(f"Here is your {order.name} ☕️. Enjoy!")
